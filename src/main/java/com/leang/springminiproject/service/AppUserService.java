d9f994758102b26d2f115b1d2dcbadc1ed56db9c
kimleang <leangk49@gmail.com>
2025-04-04 15:52:28 +0700

    auth/fix mapping

--- src/main/java/com/leang/springminiproject/service/AppUserService.java
+++ src/main/java/com/leang/springminiproject/service/AppUserService.java
@@ -1,11 +1,11 @@
 package com.leang.springminiproject.service;
 
 
+import com.leang.springminiproject.model.entity.Profile;
 import com.leang.springminiproject.model.request.AppUserRequest;
-import com.leang.springminiproject.model.response.AppUserResponse;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 
 public interface AppUserService extends UserDetailsService {
-    AppUserResponse register(AppUserRequest request);
+    Profile register(AppUserRequest request);
 }
